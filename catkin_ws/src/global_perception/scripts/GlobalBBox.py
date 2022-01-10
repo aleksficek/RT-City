eb50d751f5fafcabbdaf952adaa8cbf5cf81a5f1
FYDP Group Computer <sktuer@uwaterloo.ca>
2022-01-10 10:42:25 -0500

    more structure work

---
+++ catkin_ws/src/global_perception/scripts/GlobalBBox.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+from jsk_recognition_msgs.msg import BoundingBoxArray, BoundingBox
+
+
+class GlobalBBox():
+	def __init__(self):
+		# self.nodeNum
+        # self.id
+        # self.data - store bbox msg here
+		pass
+
+	def updateID(self):
+		pass
+
+
+class GlobalBBoxBuffer():
+    def __init__(self):
+        self.unfilteredBuffer = list()
+
+    def addNewBBox(self):
+    	pass
---
+++ catkin_ws/src/global_perception/scripts/GlobalFuser.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from jsk_recognition_msgs.msg import BoundingBoxArray, BoundingBox
+from GlobalBBox import GlobalBBoxBuffer
+from BBoxPredictor import BBoxPredictor
+
+NODE_RATE = 20 # Hz
+
+class GlobalFuser():
+    def __init__(self):
+        rospy.init_node('global_perception', anonymous=False)
+        self.rate = rospy.Rate(NODE_RATE)
+        self.sub = rospy.Subscriber('chatter', String, self.nodeCallback)
+        self.pub = rospy.Publisher('global_fused_bboxes', String, queue_size=10)
+        self.rawBuffer = GlobalBBoxBuffer() # class for recieved bbox maybe we need a double buffer?
+        self.predictions = BBoxPredictor() # class for holding predictions for ID'd bbox
+
+        # Run the fuser at 20Hz
+        while not rospy.is_shutdown():
+            self.fuserRun50ms()
+            self.rate.sleep()
+
+    def fuserRun50ms(self, event=None):
+        # process bboxes for this time step
+        self.pub.publish("Hello")
+
+    def nodeCallback(self, data):
+        # create new bbox to put in global buffer
+        pass
+        
+
+if __name__ == '__main__':
+    try:
+        GlobalFuser()
+    except rospy.ROSInterruptException:
+        pass
