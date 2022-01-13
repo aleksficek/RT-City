5ffd16c9307e9ed7273bfb36e3b493f56a239ab5
Aleksander Ficek <37374704+aleksficek@users.noreply.github.com>
2022-01-13 17:09:04 -0500

    Add Message Filtering (#4)
    * added prelim work to message filters
    
    * Add message filters
    
    * Add initial buffer
    
    * Add nested buffer
    
    * Converted to finite number of node message structure
    
    Co-authored-by: FYDP Group Computer <sktuer@uwaterloo.ca>

--- catkin_ws/src/global_perception/scripts/GlobalFuser.py
+++ catkin_ws/src/global_perception/scripts/GlobalFuser.py
@@ -3,7 +3,11 @@
 import rospy
 from std_msgs.msg import String
 from jsk_recognition_msgs.msg import BoundingBoxArray, BoundingBox
-from GlobalBBox import GlobalBBoxBuffer
+import message_filters as mf
+# from GlobalBBox import GlobalBBoxBuffer
+from global_perception.msg import GlobalBBoxBuffer
+
+from global_perception.msg import ChildGlobalBBoxBuffer
 from BBoxPredictor import BBoxPredictor
 
 NODE_RATE = 20 # Hz
@@ -12,11 +16,18 @@ class GlobalFuser():
     def __init__(self):
         rospy.init_node('global_perception', anonymous=False)
         self.rate = rospy.Rate(NODE_RATE)
-        self.sub = rospy.Subscriber('chatter', String, self.nodeCallback)
-        self.pub = rospy.Publisher('global_fused_bboxes', String, queue_size=10)
-        self.rawBuffer = GlobalBBoxBuffer() # class for recieved bbox maybe we need a double buffer?
-        self.predictions = BBoxPredictor() # class for holding predictions for ID'd bbox
 
+        self.sub_n1 = mf.Subscriber('node1/bounding_boxes', BoundingBoxArray)
+        self.sub_n2 = mf.Subscriber('node2/bounding_boxes', BoundingBoxArray)
+        self.sub_n3 = mf.Subscriber('node3/bounding_boxes', BoundingBoxArray)
+        self.time_synch = mf.TimeSynchronizer([self.sub_n1, self.sub_n2, self.sub_n3], 10)
+        self.time_synch.registerCallback(self.nodeCallback)
+        
+        self.master_buffer = GlobalBBoxBuffer()
+
+        self.pub = rospy.Publisher('global_fused_bboxes', GlobalBBoxBuffer, queue_size=10)
+        self.predictions = BBoxPredictor() # class for holding predictions for ID'd bbox
+        
         # Run the fuser at 20Hz
         while not rospy.is_shutdown():
             self.fuserRun50ms()
@@ -24,11 +35,15 @@ class GlobalFuser():
 
     def fuserRun50ms(self, event=None):
         # process bboxes for this time step
-        self.pub.publish("Hello")
+        self.pub.publish(self.master_buffer)
+        self.buffer = GlobalBBoxBuffer()
 
-    def nodeCallback(self, data):
+    def nodeCallback(self, data_n1, data_n2, data_n3):
         # create new bbox to put in global buffer
-        pass
+        self.master_buffer.node1_buffer.append(data_n1)
+        self.master_buffer.node2_buffer.append(data_n2)
+        self.master_buffer.node3_buffer.append(data_n3)
+        # self.fuserRun50ms(data_n1, data_n2, data_n3)
         
 
 if __name__ == '__main__':
