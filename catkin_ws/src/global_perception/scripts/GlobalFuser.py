5f2e3ef55255d32fb06688adf63cc0b6eef5981c
FYDP Group Computer <sktuer@uwaterloo.ca>
2022-01-13 14:04:02 -0500

    Add initial buffer

--- catkin_ws/src/global_perception/scripts/GlobalFuser.py
+++ catkin_ws/src/global_perception/scripts/GlobalFuser.py
@@ -18,23 +18,24 @@ class GlobalFuser():
         self.sub_n3 = mf.Subscriber('node3/bounding_boxes', BoundingBoxArray)
         self.time_synch = mf.TimeSynchronizer([self.sub_n1, self.sub_n2, self.sub_n3], 10)
         self.time_synch.registerCallback(self.nodeCallback)
+        self.raw_buffer = []
 
-        self.pub = rospy.Publisher('global_fused_bboxes', BoundingBoxArray, queue_size=10)
+        self.pub = rospy.Publisher('global_fused_bboxes', list, queue_size=10)
         self.predictions = BBoxPredictor() # class for holding predictions for ID'd bbox
         
         # Run the fuser at 20Hz
         while not rospy.is_shutdown():
+            self.fuserRun50ms()
             self.rate.sleep()
 
-    def fuserRun50ms(self, data_n1, data_n2, data_n3, event=None):
+    def fuserRun50ms(self, event=None):
         # process bboxes for this time step
-        self.pub.publish(data_n1)
-        self.pub.publish(data_n2)
-        self.pub.publish(data_n3)
+        self.pub.publish(self.raw_buffer)
 
     def nodeCallback(self, data_n1, data_n2, data_n3):
         # create new bbox to put in global buffer
-        self.fuserRun50ms(data_n1, data_n2, data_n3)
+        self.raw_buffer.append([data_n1, data_n2, data_n3])
+        # self.fuserRun50ms(data_n1, data_n2, data_n3)
         
 
 if __name__ == '__main__':
