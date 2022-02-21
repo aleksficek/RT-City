2c13d0af6199a820cdc9bee78f571044562891e9
FYDP Group Computer <sktuer@uwaterloo.ca>
2022-02-21 18:00:32 -0500

    Add publisher subscriber

--- catkin_ws/src/fusion/scripts/fusion_node.cpp
+++ catkin_ws/src/fusion/scripts/fusion_node.cpp
@@ -1,18 +1,46 @@
-/**
-**  Simple ROS Node
-**/
 #include <ros/ros.h>
+#include "std_msgs/String.h"
+#include <sstream>
 
-int main(int argc, char* argv[])
+class SubscribeAndPublish
 {
-  // This must be called before anything else ROS-related
-  ros::init(argc, argv, "fusion_node");
+public:
+  SubscribeAndPublish()
+  {
+    //Topic you want to publish
+    pub_ = n_.advertise<std_msgs::String>("published_topic", 1);
 
-  // Create a ROS node handle
-  ros::NodeHandle nh;
+    //Topic you want to subscribe
+    sub_ = n_.subscribe("subscribed_topic", 1, &SubscribeAndPublish::callback, this);
+  }
 
-  ROS_INFO("Hello, World!");
+  void callback(const std_msgs::String::ConstPtr& some_msg)
+  {
+    // std_msgs::String output;
+    //.... do something with the input and generate the output...
+    std_msgs::String msg;
+    std::stringstream ss;
+    ss << "hello world " << 4;
+    msg.data = ss.str();
+    pub_.publish(msg);
+  }
 
-  // Don't exit the program.
+private:
+  ros::NodeHandle n_; 
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
+
+};//End of class SubscribeAndPublish
+
+int main(int argc, char **argv)
+{
+  //Initiate ROS
+  ros::init(argc, argv, "subscribe_and_publish");
+
+  //Create an object of class SubscribeAndPublish that will take care of everything
+  SubscribeAndPublish SAPObject;
+
   ros::spin();
+
+  return 0;
 }
